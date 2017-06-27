87bde845a92918575829b6135bfd3fe3d0a48b3f
Bruno Sousa <bruno.luan.sousa@gmail.com>
2017-06-27 14:09:40 -0300

    programacaomodular package updated to run package.

--- src/run/SystemFacade.java
+++ src/run/SystemFacade.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package programacaomodular;
+package run;
 
 import account.AccountByRound;
 import account.IssueAccount;
